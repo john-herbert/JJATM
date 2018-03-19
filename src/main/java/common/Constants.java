be9173a93a974cf1621a69295d826645259800a3
john-herbert <31766132+john-herbert@users.noreply.github.com>
2018-03-19 23:10:54 +0000

    Delete Constants.java

--- src/main/java/common/Constants.java
+++
@@ -1,7 +0,0 @@
-package common;
-
-public class Constants {
-	public final static String FUNDS_ERR = "FUNDS_ERR";
-	public final static String ACCOUNTS_ERR = "ACCOUNTS_ERR";
-	public final static String ATM_ERR = "ATM_ERR";	
-}
